b9faec5b2b17a436b5988a7ca8d61e38d6fb336b
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2020-02-20 14:15:27 -0800

    Display weather data in Vue component

--- vue/src/components/Weather.vue
+++ vue/src/components/Weather.vue
@@ -1,21 +1,8 @@
 <template>
     <div>
         <h1><img alt="Vue logo" src="../assets/logo.png">eather?</h1>
-        <h3>UV Info</h3>
-        <p>The highest ultra-violet level will be {{weather.appData.uvRating}} at {{weather.appData.uvHighTime}}</p>
-
-        <h3>Heat Info</h3>
-        <p>this can be a watched property and eliminate duplication</p>
-        <p> It will be hottest at {{weather.appData.hottestTime}}. But will feel hottest at {{weather.appData.hottestApparentTime}}</p>
-        <p></p>
-
-        <h3>Air Quality</h3>
-        <p>{{weather.appData.aqi}}</p>
-
-        <h3>Is it 'supposed' to rain this week?</h3>
-        <p>{{weather.daily.summaryWeekly}}</p>
-
-
+        <!----><p>{{weather.message}}</p>
+        <p>{{weather.name}}</p>
 
     </div>
 </template>
