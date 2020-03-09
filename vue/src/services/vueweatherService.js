a77941541fff17a0ba5ac4b89cf33f489d22e9c1
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2020-03-09 14:27:38 -0700

    Notate some useful articles on JS stacks,queues, scope

--- vue/src/services/vueweatherService.js
+++ vue/src/services/vueweatherService.js
@@ -10,3 +10,10 @@ export default {
 };
 
 /* reference to path in the GET request in '/weather' is in server/src/server.js*/
+
+
+// https://crockford.com/javascript/private.html
+//http://ryanmorr.com/understanding-scope-and-context-in-javascript/
+//https://modev.net/aysnchronous-operations-nodejs/
+// https://medium.com/@Rahulx1/understanding-event-loop-call-stack-event-job-queue-in-javascript-63dcd2c71ecd
+//https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/
